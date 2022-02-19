70232b3d71fc4600294cf3398ba491462d13e244
Chiamaka Ojiyi <chiamakaojiyi@gmail.com>
2022-02-19 19:35:25 +0100

    max character in string solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let dict = {}
+
+    for(let item of str){
+        if(dict[item]){
+            dict[item] += 1
+        }else {
+            dict[item] = 1
+        }
+    }
+
+    let count = 0;
+    let maxchar;
+
+    for(let item in dict){
+        if(dict[item] > count){
+            count = dict[item]
+            maxchar = item
+        }
+    }
+    return maxchar
+}
 
 module.exports = maxChar;
