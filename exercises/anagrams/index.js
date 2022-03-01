a6383836ad4f050c56adfa409aa9808ef3a67d27
Chiamaka Ojiyi <chiamakaojiyi@gmail.com>
2022-03-01 21:52:35 +0100

    anagrams wip

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,52 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    let strA = stringA.replace(/[^A-Za-z]/g, "").toLowerCase();
+    let strB = stringB.replace(/[^A-Za-z]/g, "").toLowerCase();
+    let objA = {}
+    let objB = {}
+
+    for (let item of strA) {
+        if (objA[item]) {
+            objA[item] += 1;
+        } else {
+            objA[item] = 1;
+        }
+    }
+
+    for (let item of strB) {
+        if (objB[item]) {
+            objB[item] += 1;
+        } else {
+            objB[item] = 1;
+        }
+    }
+
+    let keysA = Object.keys(objA);
+    let keysB = Object.keys(objB);
+
+    keysA
+    let result = true;
+
+    for (let key of keysA) {
+        key
+        if (objA[key] !== objB[key]) {
+            result = false
+        }
+    }
+
+    result
+    return result;
+
+
+}
+
+let ans = anagrams('RAIL! SAFETY!', 'fairy tales')
+ans
+
+
+
+
 
 module.exports = anagrams;
