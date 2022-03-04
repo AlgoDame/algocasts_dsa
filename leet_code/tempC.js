3003890de7d51bab71de8d4bf47de36d9c60e0be
Chiamaka Ojiyi <chiamakaojiyi@gmail.com>
2022-03-04 20:05:35 +0100

    update

--- leet_code/tempC.js
+++ leet_code/tempC.js
@@ -1,13 +1,8 @@
 /**
- * askjkllksd;lse'
- * iskjslnknkl;
- * sbhoieanl,z lks
- * 
- * 
- * bklhdkjsa;ljpoupoa
- * jdbdslkj;ljpaS
- * kbjsklsejrogpfjvm,f
- * khwoioqupofn.,f;.,fjds;lrjkew
- * iuoirweyoisdfn,d.m.xvc,m vxnklrjouoiwr
- * hjhkjsdnlmn, ,.s x,.vc ,.mlkjerwojrei
+ * nklhsakopwpoupoa,n.s,dsklslkmslkxciruuityqwoikl
+ * kjksjdnlk,n, zc,.jopepoutgre
+ * lkhgeojshiosj;lnmsnlkrtuouqpoe'j
+ * hierhiohslkngfsnokjuoprgklnsdfheilwjowr
+ * hiuerkpfsdjpoeopgslknfklsupowuejl;egw
+ * ehiuhrioweyp9gn,slfdnlkjdorgd
  */
