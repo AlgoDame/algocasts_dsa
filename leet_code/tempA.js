8ecb3bbbfd6ecd9bc5421149885aee492d6f406d
Chiamaka Ojiyi <chiamakaojiyi@gmail.com>
2022-03-09 13:50:28 +0100

    update

--- leet_code/tempA.js
+++ leet_code/tempA.js
@@ -1,10 +1,3 @@
 /***
- * dflkdsj9aol;;lk
- * sbksnjl;lskjlas;lsa
- * bjsadkljf;alsj;las
- * salbaskjj90e
- * ajkjkljlv;af
- * bjkanlka;sjla
- * djbkfjpaje9e .,cv;lds
- * iuefowjmalma;loer9n,c ,.msi9re
+ * 
  */
